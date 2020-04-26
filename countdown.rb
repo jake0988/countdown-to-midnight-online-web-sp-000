3212cb56dc9bffbb64468f74d2ffc7e5101853b5
Jake G <jake098877@gmail.com>
2020-04-26 18:34:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,13 +2,13 @@
 
 def countdown(second)
   while second > 0
-    if second > 1
+    # if second > 1
       puts "#{second} SECOND(S)!"
       second -= 1
-    else
-      puts "#{second} SECOND!"
-      second -= 1
-    end
+    # else
+    #   puts "#{second} SECOND!"
+    #   second -= 1
+    # end
   end
    "HAPPY NEW YEAR!"
 end
