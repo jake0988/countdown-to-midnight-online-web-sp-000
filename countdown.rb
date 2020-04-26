904d81a9740719b634f9491a8a79c9701cb4bbf4
Jake G <jake098877@gmail.com>
2020-04-26 18:30:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,10 @@
 
 def countdown(second)
   while second > 0
-    puts "countown #{seconds}"
+    if second > 1
+      puts "#{second} SECONDS!"
+    else
+      puts "#{second} SECONDS!"
     second -= 1
   end
   puts "HAPPY NEW YEAR"
